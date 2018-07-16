79973ac069c5464d9495c78b08dff46525ee1e19
Benoît Gelineau <39692884+benoitgelineau@users.noreply.github.com>
2018-07-16 19:51:25 +0200

    Add helmet and mongodb

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+const helmet    = require('helmet');
+const mongo     = require("mongodb").MongoClient;
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -15,7 +17,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+app.use(helmet.xssFilter());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,15 +37,26 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+mongo.connect(
+  process.env.DB, // insert your database here
+  (err, db) => {
+    if (err) {
+      console.log("Database error: " + err);
+    } else {
+      console.log("Successful database connection");
+
+      //Routing for API
+      apiRoutes(app, db);
+
+      //404 Not Found Middleware
+      app.use(function(req, res, next) {
+        res.status(404)
+          .type("text")
+          .send("Not Found");
+      });
+    }
+  }
+);
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
